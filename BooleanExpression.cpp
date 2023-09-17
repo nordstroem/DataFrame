21ec07ce689b18569f1116be533575826740385d
Johan Nordström <cjs.nordstrom@gmail.com>
2023-09-17 16:17:53 +0200

    Split to hpp and cpp

--- BooleanExpression.cpp
+++ BooleanExpression.cpp
@@ -1,134 +1,88 @@
-#include <functional>
-#include <memory>
-#include <optional>
-#include <string>
+#include "BooleanExpression.hpp"
 #include "doctest.h"
 
-class ColumnName
-{
-public:
-    template <typename T>
-    ColumnName(T value)
-        : name(std::to_string(value))
-    {
-    }
-    ColumnName(const char *value)
-        : name(value)
-    {
-    }
-    const std::string name;
-};
+namespace df {
 
-ColumnName operator""_c(const char *str, std::size_t)
+ColumnName::ColumnName(const char* value)
+    : name(value)
+{
+}
+
+ColumnName operator""_c(const char* str, std::size_t)
 {
     return ColumnName(str);
 }
 
-enum class Operator
+BooleanExpression::BooleanExpression(const LeafNode& comparison)
+    : type(ExpressionType::Value)
+    , comparison(comparison)
 {
-    Equal,
-    NotEqual,
-    Less,
-    LessOrEqual,
-    Greater,
-    GreaterOrEqual,
-};
+}
 
-struct LeafNode
+BooleanExpression::BooleanExpression(ExpressionType type, std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
+    : type(type)
+    , left(std::move(left))
+    , right(std::move(right))
 {
-    LeafNode(const ColumnName &col1, Operator op, const ColumnName &col2)
-        : col1(col1), op(op), col2(col2)
-    {
-    }
+}
 
-    const ColumnName col1;
-    const Operator op;
-    const ColumnName col2;
-};
-
-enum class ExpressionType
+bool BooleanExpression::eval(std::function<bool(const std::string_view col1, Operator op, std::string_view col2)> func) const
 {
-    Value,
-    And,
-    Or
-};
-
-struct BooleanExpression
-{
-    BooleanExpression(const LeafNode &comparison)
-        : type(ExpressionType::Value), comparison(comparison)
-    {
+    switch (type) {
+    case ExpressionType::Value:
+        return func(comparison->col1.name, comparison->op, comparison->col2.name);
+    case ExpressionType::And:
+        return left->eval(func) && right->eval(func);
+    case ExpressionType::Or:
+        return left->eval(func) || right->eval(func);
     }
-    BooleanExpression(ExpressionType type, std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
-        : type(type), left(std::move(left)), right(std::move(right))
-    {
-    }
-    const ExpressionType type;
-    const std::optional<LeafNode> comparison;
-    const std::unique_ptr<BooleanExpression> left;
-    const std::unique_ptr<BooleanExpression> right;
+    return false;
+}
 
-    bool eval(std::function<bool(const std::string_view col1, Operator op, std::string_view col2)> func) const
-    {
-        switch (type)
-        {
-        case ExpressionType::Value:
-            return func(comparison->col1.name, comparison->op, comparison->col2.name);
-        case ExpressionType::And:
-            return left->eval(func) && right->eval(func);
-        case ExpressionType::Or:
-            return left->eval(func) || right->eval(func);
-        }
-        return false;
-    }
-};
-
-inline std::unique_ptr<BooleanExpression> operator&&(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
+std::unique_ptr<BooleanExpression> operator&&(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
 {
     return std::make_unique<BooleanExpression>(ExpressionType::And, std::move(left), std::move(right));
 }
 
-inline std::unique_ptr<BooleanExpression> operator||(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
+std::unique_ptr<BooleanExpression> operator||(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right)
 {
     return std::make_unique<BooleanExpression>(ExpressionType::Or, std::move(left), std::move(right));
 }
 
-inline std::unique_ptr<BooleanExpression> operator==(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator==(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::Equal, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::Equal, col2 });
 }
 
-inline std::unique_ptr<BooleanExpression> operator!=(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator!=(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::NotEqual, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::NotEqual, col2 });
 }
 
-inline std::unique_ptr<BooleanExpression> operator<(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator<(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::Less, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::Less, col2 });
 }
 
-inline std::unique_ptr<BooleanExpression> operator<=(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator<=(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::LessOrEqual, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::LessOrEqual, col2 });
 }
 
-inline std::unique_ptr<BooleanExpression> operator>(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator>(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::Greater, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::Greater, col2 });
 }
 
-inline std::unique_ptr<BooleanExpression> operator>=(const ColumnName &col1, const ColumnName &col2)
+std::unique_ptr<BooleanExpression> operator>=(const ColumnName& col1, const ColumnName& col2)
 {
-    return std::make_unique<BooleanExpression>(LeafNode(col1, Operator::GreaterOrEqual, col2));
+    return std::make_unique<BooleanExpression>(LeafNode { col1, Operator::GreaterOrEqual, col2 });
 }
 
 TEST_CASE("Boolean expressions")
 {
-    auto const standardVisitor = [](auto const &col1, Operator op, auto const &col2)
-    {
-        switch (op)
-        {
+    auto const standardVisitor = [](auto const& col1, Operator op, auto const& col2) {
+        switch (op) {
         case Operator::Equal:
             return col1 == col2;
         case Operator::NotEqual:
@@ -166,7 +120,7 @@ TEST_CASE("Boolean expressions")
         const auto expression = "x"_c == "z" || "y"_c == "z";
         CHECK_FALSE(expression->eval(standardVisitor));
     }
-    SUBCASE("two or operator - true")
+    SUBCASE("two or operator")
     {
         const auto expression = "x"_c == "z" || ("y"_c == "z" || "z"_c == "z");
         CHECK(expression->eval(standardVisitor));
@@ -176,4 +130,6 @@ TEST_CASE("Boolean expressions")
         const auto expression = "x"_c == "x" && ("y"_c == "z" || "z"_c != "z");
         CHECK_FALSE(expression->eval(standardVisitor));
     }
+}
+
 }
---
+++ BooleanExpression.hpp
@@ -0,0 +1,63 @@
+#pragma once
+#include <functional>
+#include <memory>
+#include <optional>
+#include <string>
+
+namespace df {
+class ColumnName {
+public:
+    template <typename T>
+    ColumnName(T value)
+        : name(std::to_string(value))
+    {
+    }
+    ColumnName(const char* value);
+    const std::string name;
+};
+
+ColumnName operator""_c(const char* str, std::size_t);
+
+enum class Operator {
+    Equal,
+    NotEqual,
+    Less,
+    LessOrEqual,
+    Greater,
+    GreaterOrEqual,
+};
+
+struct LeafNode {
+    const ColumnName col1;
+    const Operator op;
+    const ColumnName col2;
+};
+
+enum class ExpressionType {
+    Value,
+    And,
+    Or
+};
+
+struct BooleanExpression {
+    BooleanExpression(const LeafNode& comparison);
+    BooleanExpression(ExpressionType type, std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right);
+
+    const ExpressionType type;
+    const std::optional<LeafNode> comparison;
+    const std::unique_ptr<BooleanExpression> left;
+    const std::unique_ptr<BooleanExpression> right;
+
+    bool eval(std::function<bool(const std::string_view col1, Operator op, std::string_view col2)> func) const;
+};
+
+std::unique_ptr<BooleanExpression> operator&&(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right);
+std::unique_ptr<BooleanExpression> operator||(std::unique_ptr<BooleanExpression> left, std::unique_ptr<BooleanExpression> right);
+std::unique_ptr<BooleanExpression> operator==(const ColumnName& col1, const ColumnName& col2);
+std::unique_ptr<BooleanExpression> operator!=(const ColumnName& col1, const ColumnName& col2);
+std::unique_ptr<BooleanExpression> operator<(const ColumnName& col1, const ColumnName& col2);
+std::unique_ptr<BooleanExpression> operator<=(const ColumnName& col1, const ColumnName& col2);
+std::unique_ptr<BooleanExpression> operator>(const ColumnName& col1, const ColumnName& col2);
+std::unique_ptr<BooleanExpression> operator>=(const ColumnName& col1, const ColumnName& col2);
+
+}
